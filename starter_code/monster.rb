cd4570ca6ff361436ac5e7f27105e5214ebfac72
mrogach2350 <m.rogach@gmail.com>
2016-09-27 13:16:49 -0700

    adds monster class and values

--- starter_code/monster.rb
+++ starter_code/monster.rb
@@ -1,6 +1,43 @@
 # Monster class
+class Monster
   # getters and setters for instance variables
+  include Comparable
+  attr_accessor :habitat, :threat_level
+  @@count = 0
 
+  def initialize(habitat, threat_level = "medium")
+    @@count += 1
+    puts 'RAWR'
+    puts "#{@@count} monsters now roam the world!"
+    @habitat = habitat
+    @threat_level = threat_level
+  end
+  def habitat?(str)
+     str == @habitat
+  end
+  def self.count
+    @@count
+  end
+  def self.fight(monster1, monster2)
+    threat_array = ['low', 'medium', 'high']
+    mon_one_power = threat_array.index(monster1.threat_level)
+    mon_two_power = threat_array.index(monster2.threat_level)
+    if mon_one_power < mon_two_power
+      puts monster2
+    end
+    if mon_one_power > mon_two_power
+      puts monster1
+    end
+    if mon_one_power == mon_two_power
+      puts monster2
+    end
+  end
+end
+
+boogeyman = Monster.new('closet', "low")
+dragon = Monster.new('cave')
+
+ Monster.fight(boogeyman, dragon)
   # class variable for count
 
   # class instance variable for class description
@@ -20,7 +57,7 @@
 # Zombie class
 
   # zombie version of class_description
-  
+
   # initial behavior and values
 
 # Werewolf class
